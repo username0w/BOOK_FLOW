f604b4617f2bcadc5fb0719d990938e904e621e4
username0w <grandcias123@gmail.com>
2025-04-02 05:25:03 +0900

    first commit

---
+++ src/main/java/com/clover/bookflow/BookflowApplication.java
@@ -0,0 +1,13 @@
+package com.clover.bookflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookflowApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookflowApplication.class, args);
+  }
+
+}
