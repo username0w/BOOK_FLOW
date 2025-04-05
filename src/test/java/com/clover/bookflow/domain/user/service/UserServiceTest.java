d5d4980fd572262590a74ed6a92605aba8041f39
username0w <grandcias123@gmail.com>
2025-04-06 03:08:20 +0900

    test(user): UserServiceTest 리팩토링 - User 생성 시 ID 제거 및 테스트 개선

--- src/test/java/com/clover/bookflow/domain/user/service/UserServiceTest.java
+++ src/test/java/com/clover/bookflow/domain/user/service/UserServiceTest.java
@@ -2,11 +2,13 @@ package com.clover.bookflow.domain.user.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 import com.clover.bookflow.domain.user.dto.request.UserSignupRequest;
 import com.clover.bookflow.domain.user.dto.response.UserSignupResponse;
 import com.clover.bookflow.domain.user.entity.User;
+import com.clover.bookflow.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,17 +29,18 @@ public class UserServiceTest {
   @DisplayName("회원 가입 성공 테스트")
   void signup() {
     // given
-    UserSignupRequest request = new UserSignupRequest("길똥이", "hkd111@example.com",
-        "password123");
-    User expectedUser = new User(1L, "길똥이", "hkd111@example.com", "password123");
+    UserSignupRequest request = new UserSignupRequest("hkd111@example.com",
+        "password123", "길똥이");
+    User expectedUser = new User("hkd111@example.com", "password123", "길똥이");
+    // id 없이 테스트 가능
     given(userRepository.save(any(User.class))).willReturn(expectedUser);
 
     // when
     UserSignupResponse response = userService.signup(request);
 
     // then
-    assertThat(response.getNickname()).isEqualTo("길똥이");
-    assertThat(response.getEmail()).isEqualTo("hkd111@example.com");
+    assertThat(response.nickname()).isEqualTo("길똥이");
+    assertThat(response.email()).isEqualTo("hkd111@example.com");
   }
 
   // 이메일 중복시 회원 가입 실패
@@ -45,7 +48,7 @@ public class UserServiceTest {
   @DisplayName("이미 가입된 이메일로 회원 가입하면 예외가 발생한다.")
   void should_ThrowException_When_EmailAlreadyExists() {
     // given
-    UserSignupRequest userSignupRequest = new UserSignupRequest("홍길동", "hkd111@example.com",
+    UserSignupRequest userSignupRequest = new UserSignupRequest("hkd111@example.com",
         "password123", "길똥이");
     given(userRepository.existsByEmail("hkd111@example.com")).willReturn(true);
 
