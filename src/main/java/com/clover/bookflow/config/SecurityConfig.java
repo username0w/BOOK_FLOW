6b6bf83c641077ae3656261cddc5aebd52457749
username0w <163955522+username0w@users.noreply.github.com>
2025-06-08 22:37:42 +0900

    refactor(security): JwtAuthenticationFilter에 토큰 처리 로직 이동 및 화이트리스트 예외 처리 추가
    - JwtProvider에서 토큰 추출 로직을 JwtAuthenticationFilter로 이동
    - JwtAuthenticationFilter에 경로 화이트리스트 예외 처리 추가하여 필터 체인 흐름 개선
    - SecurityConfig에서 세션 정책 및 경로 설정 수정

---
+++ src/main/java/com/clover/bookflow/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.clover.bookflow.config;
+
+import com.clover.bookflow.domain.auth.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        ) // Spring 이 세션 만들지 않게 한다.
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/docs/**").permitAll()
+            .requestMatchers("/api/v1/auth/signup").permitAll()
+            .anyRequest().authenticated()
+        )
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .formLogin(form -> form.disable())
+        .httpBasic(basic -> basic.disable());
+
+    return http.build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration configuration
+  ) throws Exception {
+    return configuration.getAuthenticationManager(); // Spring 이 제공하는 AuthenticationManager
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+
+}
