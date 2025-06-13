5770eda52ca52ec9263a561d52c1b79d3982d550
username0w <163955522+username0w@users.noreply.github.com>
2025-06-13 23:40:10 +0900

    refactor(error): 로그인 관련 에러 코드 통합 (비밀번호/이메일 오류)

--- src/main/java/com/clover/bookflow/global/errorcode/UserErrorCode.java
+++ src/main/java/com/clover/bookflow/global/errorcode/UserErrorCode.java
@@ -8,7 +8,7 @@ public enum UserErrorCode implements ErrorCode {
 
   USER_NOT_FOUND("USER_001", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
   INVALID_USER_STATE("USER_002", "사용자 상태가 유효하지 않습니다.", HttpStatus.BAD_REQUEST),
-  PASSWORD_MISMATCH("USER_003", "비밀번호가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED),
+  LOGIN_FAILED("USER_003", "아이디 또는 비밀번호가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED),
 
   EMAIL_ALREADY_EXISTS("USER_004", "이미 등록된 이메일입니다.", HttpStatus.CONFLICT),
   NICKNAME_ALREADY_EXISTS("USER_005", "이미 존재하는 닉네임입니다.", HttpStatus.CONFLICT);
