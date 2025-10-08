729f205dd71372edb1f6e172e55c568dcee0c4d7
username0w <163955522+username0w@users.noreply.github.com>
2025-10-08 21:22:54 +0900

    feat(readbook): error code 추가

---
+++ src/main/java/com/clover/bookflow/global/errorcode/ReadBookErrorCode.java
@@ -0,0 +1,24 @@
+package com.clover.bookflow.global.errorcode;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ReadBookErrorCode implements ErrorCode {
+
+  READ_BOOK_NOT_FOUND("READ_BOOK_001", "읽은 책을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+  INVALID_ISBN("READ_BOOK_002", "유효하지 않은 ISBN입니다.", HttpStatus.BAD_REQUEST),
+  DUPLICATE_READ_BOOK("READ_BOOK_003", "이미 등록된 읽은 책입니다.", HttpStatus.CONFLICT);
+
+  private final String code;
+  private final String message;
+  private final HttpStatus status;
+
+  // enum은 특별한 클래스라서, 생성자를 명시적으로 작성하는 게 가독성에 좋고, Lombok 어노테이션과 충돌 가능성도 줄어듦
+  ReadBookErrorCode(String code, String message, HttpStatus status) {
+    this.code = code;
+    this.message = message;
+    this.status = status;
+  }
+
+}
